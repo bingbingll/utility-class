d17c0a2936c9d5623e5937113ed387b28eee1b02
bing.li <ibinbin68@163.com>
2019-12-10 12:44:04 +0800

    创建

--- src/main/java/com/example/utility/MapUtil.java
+++ src/main/java/com/example/utility/MapUtil.java
@@ -12,13 +12,11 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 
 @SuppressWarnings("all")
@@ -31,21 +29,6 @@ public class MapUtil {
      * @author 李兵
      * @date 2019/6/22 16:29
      **/
-    @Deprecated
-    public static Map<String, Object> transformUpperCaseByMap(Map<String, Object> orgMap) {
-        Map<String, Object> resultMap = new HashMap<>();
-        if (orgMap.isEmpty() || orgMap.equals(null)) {
-            return resultMap;
-        }
-        Set<String> keySet = orgMap.keySet();
-        for (Object key : keySet) {
-            String newKey = key.toString().toLowerCase();
-            newKey = newKey.replace("_", "");
-            resultMap.put(newKey, orgMap.get(key));
-        }
-        return resultMap;
-    }
-
     public static <K, V> Map<K, V> newTransformUpperCaseByMap(@NotNull Map<K, V> orgMap) {
         Map<K, V> resultMap = new HashMap<>();
         if (ObjectUtils.isEmpty(orgMap)) {
@@ -63,25 +46,6 @@ public class MapUtil {
         return resultMap;
     }
 
-    /**
-     * TODO: list map key转换为小写
-     *
-     * @param orgListMap :
-     * @return void
-     * @author 李兵
-     * @date 2019/6/22 16:28
-     **/
-    @Deprecated
-    public static List<Map<String, Object>> transformUpperCaseByList(List<Map<String, Object>> orgListMap) {
-        List<Map<String, Object>> res = new ArrayList<>();
-        if (orgListMap.size() <= 0) {
-            return res;
-        }
-        for (Map<String, Object> objectObjectMap : orgListMap) {
-            res.add(transformUpperCaseByMap(objectObjectMap));
-        }
-        return res;
-    }
 
     public static <K, V> List<Map<K, V>> newTransformUpperCaseByList(List<Map<K, V>> arrayList) {
         List<Map<K, V>> res = new ArrayList<>();
@@ -122,108 +86,6 @@ public class MapUtil {
     }
 
 
-    /**
-     * TODO: list 中map重组
-     *
-     * @param lmps   : 数据库中查询的数据
-     * @param xNames : 定义x轴的名称
-     * @return java.util.List
-     * @author 李兵
-     * @date 2019/6/20 18:20
-     **/
-    public static List<Map<String, Object>> congZhu(List<LinkedHashMap<String, Object>> lmps, Object[] xNames, int xnameCount) {
-        List<Map<String, Object>> rests = new ArrayList<>();
-
-        for (Object name : xNames) {
-            LinkedHashMap<String, Object> linkedHashMap = ist(lmps, name, xnameCount);
-            if (ObjectUtils.isNotEmpty(linkedHashMap)) {
-                rests.add(linkedHashMap);
-            }
-        }
-        return rests;
-    }
-
-    /**
-     * TODO: 验证list中的map对象是否存指定的值
-     *
-     * @param lmps :
-     * @param name :
-     * @return java.util.LinkedHashMap
-     * @author 李兵
-     * @date 2019/6/20 18:16
-     **/
-    private static LinkedHashMap<String, Object> ist(List<LinkedHashMap<String, Object>> lmps, Object name, int xnameCount) {
-        //定义一个空的对象
-        LinkedHashMap<String, Object> linkedHashMap = new LinkedHashMap<String, Object>(1);
-        boolean ist = true;
-        Set<String> keySet = new HashSet();
-
-        for (LinkedHashMap lmp : lmps) {
-            keySet = lmp.keySet();
-            if (lmp.containsValue(name)) {
-                ist = false;
-                // TODO: 注意此次为深度克隆，不能使用=复制，putAll 等。若原始map的值发生变化，目标map的值也会变化。
-                linkedHashMap = MapUtil.clone(lmp);
-                break;
-            }
-        }
-        //循环所有的list中map 发现没有存在当前值时进行存放
-        if (ist) {
-            Iterator<String> iterator1 = keySet.iterator();
-            if (!iterator1.hasNext()) {
-                //进入表示 lmps 集合中没有数据
-                linkedHashMap.put("xname", name);
-                switch (xnameCount) {
-                    case 1:
-                        linkedHashMap.put("val1", 0L);
-                        break;
-                    case 2:
-                        linkedHashMap.put("val1", 0L);
-                        linkedHashMap.put("val2", 0L);
-                        break;
-                    case 3:
-                        linkedHashMap.put("val1", 0L);
-                        linkedHashMap.put("val2", 0L);
-                        linkedHashMap.put("val3", 0L);
-                        break;
-                    case 4:
-                        linkedHashMap.put("val1", 0L);
-                        linkedHashMap.put("val2", 0L);
-                        linkedHashMap.put("val3", 0L);
-                        linkedHashMap.put("val4", 0L);
-                        break;
-                    default:
-                        linkedHashMap.put("val", 0);
-                        break;
-                }
-            }
-            while (iterator1.hasNext()) {
-                String nextKey = iterator1.next();
-                //列名缺少补充
-                if ("xname".equals(nextKey)) {
-                    linkedHashMap.put(nextKey, name);
-                } else if ("status".equals(nextKey)) {
-                    linkedHashMap.put(nextKey, name);
-                } else if ("name".equals(nextKey)) {
-                    linkedHashMap.put(nextKey, name);
-                } else {
-                    linkedHashMap.put(nextKey, 0);
-                }
-            }
-        }
-        return linkedHashMap;
-    }
-
-
-    /**
-     * TODO:
-     *
-     * @param list      :
-     * @param KeyLenght :
-     * @return java.lang.Object[][]
-     * @author 李兵
-     * @date 2019/6/21 10:20
-     **/
     public static Object[][] listToArray(List<LinkedHashMap<String, Object>> list, int KeyLenght) {
         if (CollectionUtils.isEmpty(list)) {
             return new Object[0][];
@@ -236,37 +98,6 @@ public class MapUtil {
             array[i] = list.get(i).values().toArray();
         }
         return array;
-    }
-
-    /**
-     * 获取title
-     *
-     * @param queryDatas
-     * @return
-     */
-    public static List<String> getTitile(List<LinkedHashMap<String, Object>> queryDatas, String key) {
-        Set<String> collect = queryDatas.stream().map(e -> e.get(key) + "").collect(Collectors.toSet());
-        return new ArrayList<String>(collect);
-    }
-
-    public static List<Map<String, Object>> congZhu2(List<LinkedHashMap<String, Object>> queryDatas, List<String> titles,
-                                                     Object[] xNames, String key) {
-        List<Map<String, Object>> rests = new ArrayList<>();
-        for (String title : titles) {
-            for (Object name : xNames) {
-                LinkedHashMap<String, Object> orElse = queryDatas.stream().filter(e -> e.containsValue(title) && e.containsValue(name)).findFirst().orElse(null);
-                if (orElse != null) {
-                    rests.add(orElse);
-                } else {
-                    LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
-                    map.put("CREATE_DATE", name);
-                    map.put("PARENT_NAME", title);
-                    map.put(key, 0);
-                    rests.add(map);
-                }
-            }
-        }
-        return rests;
     }
 
 
@@ -343,7 +174,7 @@ public class MapUtil {
     }
 
     public static <T> List<T> lkmapsToObjects(List<LinkedHashMap<String, Object>> lkmaps, Class<T> clazz) {
-        List<T> list = new ArrayList<>();
+        LinkedList<T> list = new LinkedList<>();
         if (lkmaps != null && lkmaps.size() > 0) {
             lkmaps.forEach(x -> {
                 T bean = null;
